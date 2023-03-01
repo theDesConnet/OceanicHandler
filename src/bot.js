27679712a94b12c4ce96e1efc98a177341786862
Roman U <uporov.201125@gmail.com>
2023-03-01 18:18:34 +0700

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -1,4 +1,4 @@
 const Client = require('./structures/client');
 const config = require('./jsons/config.json');
 
-new Client({auth: `Bot MTAzMzM4NTg4MzgyMDM2Mzg4Ng.GxQzLJ.6QO0hOGjptau5eVG7Z4ZjfdLsOciOWOdC_5b10`});
+new Client({auth: `Bot ${config.token}`});
